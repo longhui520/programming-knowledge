bdfff48b3f1d33ec46daa9a2a738c12e87414826
longhui <625243422@qq.com>
2020-07-31 11:49:31 +0800

    工具栏自动生成

--- src/tools/index.js
+++ src/tools/index.js
@@ -1,8 +1,9 @@
-// 常用工具生成函数
+// 根据json生成 常用工具栏文档
 const fs = require("fs")
+const path = require("path")
 const pretty = require("pretty")
 const template = require("art-template")
-const templateStr = fs.readFileSync('template.html').toString()
-const json = JSON.parse(fs.readFileSync('data.json').toString())
-// console.log(template.render(templateStr,{groups:json}))
-fs.writeFileSync('1.md',pretty(template.render(templateStr,{groups:json})))
+const templateStr = fs.readFileSync(path.join(__dirname,'template.html')).toString()
+const json = JSON.parse(fs.readFileSync(path.join(__dirname,'data.json')).toString())
+const rootPath=path.join(__dirname,'../../');
+fs.writeFileSync(path.join(rootPath,'docs/tools/README.md'),pretty(template.render(templateStr,{groups:json})))
