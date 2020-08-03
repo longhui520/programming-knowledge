059763ba72750ff3e60ad0c435c1a511e8980019
longhui520 <39285837+longhui520@users.noreply.github.com>
2020-08-03 22:43:01 +0800

    增加文章列

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,12 @@ module.exports = {
     logo: '/assets/images/logo.png',
     nav: [
       { text:'常用工具',link:'/tools/'},
+      { text:'学习文章',items:[
+        {
+          text:'vue源码系列',
+          link:'/article/vue/'
+        }
+      ]},
       { text: 'vuepress', link: 'https://www.vuepress.cn/' }
     ]
   }
---
+++ docs/article/vue/README.md
@@ -0,0 +1,2 @@
+# vue
+- [vue diff 原理及流程](https://juejin.im/post/6844903961837699079)
