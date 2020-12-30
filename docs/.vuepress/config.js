929334022ea06525f233cc1992a49069efa29848
longhui <625243422@qq.com>
2020-12-30 11:50:35 +0800

    启用github工作 流

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "龙辉",
   description: "a collection of programming knowledge",
-  base:"/knowledge/",
+  base:"/programming-knowledge/",
   themeConfig: {
     logo: '/assets/images/logo.png',
     nav: [
